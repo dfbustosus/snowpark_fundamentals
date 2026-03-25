af52bd31fb86bff921aec6c2e1ca1ed609b1c987
David BU <dfbustos@uninorte.edu.co>
2026-03-25 10:16:09 -0300

    Update data module

--- src/snowpark_fundamentals/data/__init__.py
+++ src/snowpark_fundamentals/data/__init__.py
@@ -1,6 +1,6 @@
 """Data loading and sample data generation utilities."""
 
-from snowpark_fundamentals.data.loader import load_table, load_with_sql
+from snowpark_fundamentals.data.loader import load_table, load_with_sql, sample_stratified
 from snowpark_fundamentals.data.sample_data import (
     create_customer_churn_dataset,
     create_sample_customers_dataset,
@@ -10,6 +10,7 @@ from snowpark_fundamentals.data.sample_data import (
 __all__ = [
     "load_table",
     "load_with_sql",
+    "sample_stratified",
     "create_customer_churn_dataset",
     "create_sample_customers_dataset",
     "create_sample_orders_dataset",
